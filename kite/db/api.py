be7828a26a8acd98e650e46e1755779d2d051463
Jamie Lennox <jamielennox@redhat.com>
2013-12-03 12:33:28 +1000

    Introduce database functionality into KDS
    Add database setup and initial testing into Key Distribution Server.
    Also adds a migration script.
    
    Change-Id: I22e914ef1ca6ec8217d131c073261b309d9f637e

---
+++ kite/db/api.py
@@ -0,0 +1,40 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo.config import cfg
+
+from kite.openstack.common.db import api as db_api
+
+CONF = cfg.CONF
+IMPL = CONF.database
+
+CONF.import_opt('backend',
+                'kite.openstack.common.db.options',
+                group='database')
+
+_BACKEND_MAPPING = {'sqlalchemy': 'kite.db.sqlalchemy.api',
+                    'kvs': 'kite.db.kvs.api'}
+
+
+def reset():
+    global IMPL
+    IMPL = None
+
+
+def get_instance(force_new=False):
+    """Return a DB API instance."""
+    global IMPL
+    if not IMPL or force_new:
+        IMPL = db_api.DBAPI(CONF.database.backend,
+                            backend_mapping=_BACKEND_MAPPING)
+
+    return IMPL
---
+++ kite/tests/db/base.py
@@ -0,0 +1,44 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+
+from kite.db.kvs import api as kvs_api
+from kite.db.sqlalchemy import api as sql_api
+from kite.db.sqlalchemy import migration
+from kite.tests import base
+
+connection = os.getenv('OS_TEST_DBAPI_CONNECTION', 'sqlite://')
+
+# TODO(jamielennox): there is some support in OSLO for migration tests and
+# opportunistic tests. We should do more extensive real testing.
+
+
+class BaseTestCase(base.BaseTestCase):
+
+    scenarios = [('sqlitedb', {'connection': connection,
+                               'backend': sql_api}),
+                 ('kvsdb', {'connection': None,
+                            'backend': kvs_api})
+                 ]
+
+    def setUp(self):
+        super(BaseTestCase, self).setUp()
+        self.backend.reset()
+
+        if self.connection:
+            self.config_fixture.config(connection=self.connection,
+                                       group='database')
+            migration.upgrade()
+            self.addCleanup(migration.downgrade)
+
+        self.DB = self.backend.get_backend()
