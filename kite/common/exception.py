e4bf84358f72482da9d1cc37d56a31607bd8be85
Ronald Bradford <ronald.bradford@gmail.com>
2016-03-15 15:51:58 -0400

    Graduate to oslo.i18n messages
    NOTE: we cannot remove openstack/common/gettextutils until we have
    removed other Oslo incubator code that uses it.
    
    Implements: blueprint graduate-oslo-i18n [1]
    https://blueprints.launchpad.net/oslo-incubator/+spec/graduate-oslo-i18n
    
    Change-Id: Ib8a34e7b9912f8658b324126b3f353dcd7d97d27

--- kite/common/exception.py
+++ kite/common/exception.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from kite.openstack.common.gettextutils import _  # noqa
+from kite.common._i18n import _  # noqa
 
 _FATAL_EXCEPTION_FORMAT_ERRORS = False
 
